2cc3fb83d222e9df3890f27e0e3d1f5e297edfa9
u0861355 <u0861355@utah.edu>
2020-10-02 13:29:22 -0600

    replace old 2d project with 3d project and add very basic navigation scene

---
+++ DadaWarGame/Assets/FollowPath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPath : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] wayPoints;
+    NavMeshAgent agent;
+
+    int destinationIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    public void GoToDestination(int index)
+    {
+        agent.SetDestination(wayPoints[index].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance < 0.1)
+        {
+            destinationIndex++;
+            destinationIndex = destinationIndex % wayPoints.Length;
+            GoToDestination(destinationIndex);
+        }
+    }
+}
