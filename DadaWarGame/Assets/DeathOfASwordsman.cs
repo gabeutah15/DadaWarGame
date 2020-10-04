ffba6655681660fcffb9daf4dad3962d46a5e42b
u0861355 <u0861355@utah.edu>
2020-10-03 23:27:16 -0600

    added a dead body model for swordsman that falls over and disappears

---
+++ DadaWarGame/Assets/DeathOfASwordsman.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathOfASwordsman : MonoBehaviour
+{
+    bool isDead = false;
+    float elapsedTimeSinceDeath;
+    [SerializeField]
+    float timeUntilBodyDisappears = 5;
+
+    private void Awake()
+    {
+        isDead = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isDead)
+        {
+            elapsedTimeSinceDeath += Time.deltaTime;
+            if (elapsedTimeSinceDeath > timeUntilBodyDisappears)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
--- DadaWarGame/Assets/EnemyAIBody.cs
+++ DadaWarGame/Assets/EnemyAIBody.cs
@@ -12,7 +12,9 @@ public class EnemyAIBody : MonoBehaviour
     NavMeshAgent agent;
     GameObject[] playerAgents;
     GameObject currentTarget;
-
+    [SerializeField]
+    GameObject deathModelPrefab;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -124,6 +126,13 @@ public class EnemyAIBody : MonoBehaviour
         if (health <= 0)
         {
             this.gameObject.SetActive(false);
+            if (deathModelPrefab)
+            {
+                GameObject thisUnitDead = Instantiate(deathModelPrefab, this.transform.position, this.transform.rotation) as GameObject;
+                thisUnitDead.GetComponent<Rigidbody>().AddForce(new Vector3(1f,0,0.5f));//just enough to knock it over
+                
+            }
+
             //Destroy(this.gameObject);
         }
     }
