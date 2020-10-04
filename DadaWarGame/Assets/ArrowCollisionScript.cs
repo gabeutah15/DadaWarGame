c8ac44e620933ff195135100f4c468122c3fd6ea
u0861355 <u0861355@utah.edu>
2020-10-03 19:27:29 -0600

    projectileparentnull check

--- DadaWarGame/Assets/ArrowCollisionScript.cs
+++ DadaWarGame/Assets/ArrowCollisionScript.cs
@@ -9,24 +9,28 @@ public class ArrowCollisionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        projectileParent = GetComponentInParent<Projectile>();
+        projectileParent = GetComponentInParent<Projectile>();//sometimes this is null but it shouldn't be?
         rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         //don't collide with other arrows
-        if (!collision.gameObject.GetComponentInParent<Projectile>() && (projectileParent.elapsedTime > 0.1f))
+        if (collision.gameObject && projectileParent)
         {
-            //attempt to make arrows get stuck in target
-            if ((collision.gameObject.GetComponent<AIControl>() || collision.gameObject.GetComponent<EnemyAIBody>()) && (projectileParent.elapsedTime > 0.15f))
+            if (!collision.gameObject.GetComponentInParent<Projectile>() && (projectileParent.elapsedTime > 0.1f))
             {
-                rb.isKinematic = true;
-                rb.useGravity = false;
-                projectileParent.speed = 0;
-                projectileParent.transform.parent = collision.gameObject.transform;
+                //attempt to make arrows get stuck in target
+                //issues may be caused by projectileparent sometimes being null?
+                if ((collision.gameObject.GetComponent<AIControl>() || collision.gameObject.GetComponent<EnemyAIBody>()) && (projectileParent.elapsedTime > 0.15f))
+                {
+                    rb.isKinematic = true;
+                    rb.useGravity = false;
+                    projectileParent.speed = 0;
+                    projectileParent.transform.parent = collision.gameObject.transform;
+                }
+                projectileParent.isFlying = false;
             }
-            projectileParent.isFlying = false;
         }
     }
 }
