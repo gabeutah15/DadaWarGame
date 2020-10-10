aa2b90d881c28b1b3f354be411be7a22503cede8
joshuaffc <joshuacheng123@outlook.com>
2020-10-10 01:07:27 -0600

    add UI scripts

---
+++ DadaWarGame/Assets/Scripts/MessagerUIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagerUIManager : MonoBehaviour
+{
+    public Image M1;
+    public Image M2;
+    public Image M3;
+    public Image M4;
+    public Image M5;
+    public int MessagerNum;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MessagerNum == 5)
+        {
+            M1.gameObject.SetActive(true);
+            M2.gameObject.SetActive(true);
+            M3.gameObject.SetActive(true);
+            M4.gameObject.SetActive(true);
+            M5.gameObject.SetActive(true);
+        }
+        if (MessagerNum == 4)
+        {
+            M1.gameObject.SetActive(true);
+            M2.gameObject.SetActive(true);
+            M3.gameObject.SetActive(true);
+            M4.gameObject.SetActive(true);
+            M5.gameObject.SetActive(false);
+        }
+        if (MessagerNum == 3)
+        {
+            M1.gameObject.SetActive(true);
+            M2.gameObject.SetActive(true);
+            M3.gameObject.SetActive(true);
+            M4.gameObject.SetActive(false);
+            M5.gameObject.SetActive(false);
+        }
+        if (MessagerNum == 2)
+        {
+            M1.gameObject.SetActive(true);
+            M2.gameObject.SetActive(true);
+            M3.gameObject.SetActive(false);
+            M4.gameObject.SetActive(false);
+            M5.gameObject.SetActive(false);
+        }
+        if (MessagerNum == 1)
+        {
+            M1.gameObject.SetActive(true);
+            M2.gameObject.SetActive(false);
+            M3.gameObject.SetActive(false);
+            M4.gameObject.SetActive(false);
+            M5.gameObject.SetActive(false);
+        }
+        if (MessagerNum == 0)
+        {
+            M1.gameObject.SetActive(false);
+            M2.gameObject.SetActive(false);
+            M3.gameObject.SetActive(false);
+            M4.gameObject.SetActive(false);
+            M5.gameObject.SetActive(false);
+        }
+
+
+    }
+}
---
+++ DadaWarGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public ProgressBar PB;
+    public MessagerUIManager MUIM;
+    //public GameObject SpawnOBJ;
+    //RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update messagerNum
+        MUIM.MessagerNum = 4;
+        //update progressBar
+        PB.current = 50;
+        PB.maximum = 100;
+
+        //spawn characters
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    if (EventSystem.current.IsPointerOverGameObject())
+        //    {
+        //        Debug.Log("touch area is UI");
+        //    }
+        //    else
+        //    {
+        //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //        if (Physics.Raycast(ray, out hit))
+        //        {
+        //            Debug.Log("123");
+        //            Instantiate(SpawnOBJ, hit.point, transform.rotation);
+        //        }
+        //    }
+        //}
+
+    }
+}
